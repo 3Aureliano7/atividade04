7296fa3531de9872d68cd31a17a62883cce83b40
Rodney <rodney.07.ramos@gmail.com>
2023-09-13 21:29:44 -0300

    entrega part 4 atividade 4

--- part04/atividade44.js
+++ part04/atividade44.js
@@ -3,6 +3,16 @@ import PromptSync from "prompt-sync";
 
 let nome=prompt("qual seu nome? ")
 
-while (nome=='') {
+while ( nome == '' ) {
         nome=prompt("qual seu nome? ")
 }
+let hobbie=[]
+
+        let out = true
+        for (let i = 0; i >= i; i++) {
+                hobbie.push(prompt('Digite um de seus hobbies: '))   
+                console.log(i)
+                if(hobbie[i] == 0){out = false;break}
+        }
+hobbie.pop(hobbie.lengh-1)
+console.log(`${nome}|${hobbie}`)
